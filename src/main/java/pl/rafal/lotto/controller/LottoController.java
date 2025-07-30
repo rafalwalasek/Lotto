14ac5e85f1022cda23509e8399fde3ee36d78ddf
Rafal Walasek <magician1590@gmail.com>
2025-07-30 10:28:59 +0200

    sprawdzanie wieku i poprawki

--- src/main/java/pl/rafal/lotto/controller/LottoController.java
+++ src/main/java/pl/rafal/lotto/controller/LottoController.java
@@ -1,9 +1,11 @@
 package pl.rafal.lotto.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import pl.rafal.lotto.model.Player;
 import pl.rafal.lotto.repository.PlayerRepository;
 
@@ -21,7 +23,12 @@ public class LottoController {
     }
 
     @PostMapping("/save-to-db")
-    public String saveToDB(@ModelAttribute Player player) {
+    public String saveToDB(@ModelAttribute Player player, Model model) {
+        if (player.getAge() < 18) {
+            model.addAttribute("error", "Musisz mieć conajmniej 18 lat aby dołączyć do gry!");
+            return "index";
+        }
+
         playerRepository.save(player);
         return "save-to-db";
     }
