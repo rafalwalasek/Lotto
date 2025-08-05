b6328ea1838b3420239448c53a8095ed16825f07
Rafal Walasek <magician1590@gmail.com>
2025-08-05 19:03:52 +0200

    poprawki i wyswietlanie imienia uzytkownika w panelu glownym

--- src/main/java/pl/rafal/lotto/controller/LottoController.java
+++ src/main/java/pl/rafal/lotto/controller/LottoController.java
@@ -1,11 +1,13 @@
 package pl.rafal.lotto.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.rafal.lotto.model.Player;
 import pl.rafal.lotto.repository.PlayerRepository;
 
@@ -21,24 +23,48 @@ public class LottoController {
     public String index() {
         return "index";
     }
-    @GetMapping("/user-panel")
-    public String userPanel(Model model) {
-        Player player = playerRepository.findAll().getFirst();
-        model.addAttribute("player", player);
-        return "user-panel";
-    }
 
     @PostMapping("/save-to-db")
     public String saveToDB(@ModelAttribute Player player, Model model) {
         if (player.getAge() < 18) {
-            model.addAttribute("error", "Musisz mieć conajmniej 18 lat aby dołączyć do gry!");
+            model.addAttribute("registerError", "Musisz mieć conajmniej 18 lat aby dołączyć do gry!");
             return "index";
         } else if (player.getName().isBlank() || player.getEmail().isBlank() || player.getPassword().isBlank()) {
-            model.addAttribute("error", "Wszystkie pola są wymagane!");
+            model.addAttribute("registerError", "Wszystkie pola są wymagane!");
+            return "index";
+        }
+
+        if (playerRepository.existsByEmail(player.getEmail())) {
+            model.addAttribute("registerError", "Użytkownik z takim e-mailem już istnieje.");
             return "index";
         }
 
         playerRepository.save(player);
         return "save-to-db";
+    }
+
+    @GetMapping("/user-panel")
+    public String userPanel(HttpSession session, Model model) {
+        Player player = (Player) session.getAttribute("loggedPlayer");
+
+        if (player == null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("player", player);
+        return "user-panel";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Player player, HttpSession session, Model model) {
+        Player dbPlayer = playerRepository.findByEmail(player.getEmail());
+
+        if (dbPlayer == null || !dbPlayer.getPassword().equals(player.getPassword())) {
+            model.addAttribute("loginError", "Nieprawidłowy email lub hasło!");
+            return "index";
+        }
+
+        session.setAttribute("loggedPlayer", dbPlayer);
+        return "redirect:/user-panel";
     }
 }
