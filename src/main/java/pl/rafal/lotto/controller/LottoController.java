ff1e1bd977e5492146e9126cf826d647e1c59ea3
Rafal Walasek <magician1590@gmail.com>
2025-08-25 11:53:29 +0200

    poprawki, stylizacja i utworzenie panelu uzytkownika

--- src/main/java/pl/rafal/lotto/controller/LottoController.java
+++ src/main/java/pl/rafal/lotto/controller/LottoController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.rafal.lotto.model.Player;
 import pl.rafal.lotto.repository.PlayerRepository;
 
